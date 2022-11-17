1c8832c29e1c0b0c82971fd17bc834537062a2e9
Gideon Kiplangat <56033401+giddy22-02@users.noreply.github.com>
2022-11-17 12:25:07 +0300

    database

--- DATABASE FILE/real_estate.sql
+++ DATABASE FILE/real_estate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2022 at 05:04 PM
+-- Generation Time: Nov 17, 2022 at 09:22 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -39,7 +39,7 @@ CREATE TABLE `about` (
 --
 
 INSERT INTO `about` (`id`, `title`, `content`, `image`) VALUES
-(10, 'Get to Know Us', '<div id=\"pgc-w5d0dcc3394ac1-0-0\" class=\"panel-grid-cell\">\r\n<div id=\"panel-w5d0dcc3394ac1-0-0-0\" class=\"so-panel widget widget_sow-editor panel-first-child panel-last-child\" data-index=\"0\">\r\n<div class=\"so-widget-sow-editor so-widget-sow-editor-base\">\r\n<div class=\"siteorigin-widget-tinymce textwidget\">\r\n<p class=\"text_all_p_tag_css\">We are the best property development company, passionate about providing quality comfortable<br />homes to institutions and private investors by presenting a unique value proposition. We are the<br />leading trusted providers of housing solutions in Kenya; on a mission to construct affordable and<br />quality residential housing in Kenya for both home seekers and institutional investors. Our vision<br />is to make homeownership affordable, seamless, and accessible to all.</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n<p class=\"text_all_p_tag_css\">IKran IKRAN IKRAN</p>\r\n</div>\r\n</div>\r\n</div>\r\n</div>', 'banner 5.jpg');
+(10, 'Get to Know Us', '<div id=\"pgc-w5d0dcc3394ac1-0-0\" class=\"panel-grid-cell\">\r\n<div id=\"panel-w5d0dcc3394ac1-0-0-0\" class=\"so-panel widget widget_sow-editor panel-first-child panel-last-child\" data-index=\"0\">\r\n<div class=\"so-widget-sow-editor so-widget-sow-editor-base\">\r\n<div class=\"siteorigin-widget-tinymce textwidget\">\r\n<p class=\"text_all_p_tag_css\">We are the best property development company, passionate about providing quality comfortable<br />homes to institutions and private investors by presenting a unique value proposition. We are the<br />leading trusted providers of housing solutions in Kenya; on a mission to construct affordable and<br />quality residential housing in Kenya for both home seekers and institutional investors. Our vision<br />is to make homeownership affordable, seamless, and accessible to all.</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n<p class=\"text_all_p_tag_css\">test</p>\r\n<p class=\"text_all_p_tag_css\">test</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n<p class=\"text_all_p_tag_css\">&nbsp;</p>\r\n</div>\r\n</div>\r\n</div>\r\n</div>', 'pexels-photo-3888151.jpeg');
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`aid`, `auser`, `aemail`, `apass`, `adob`, `aphone`) VALUES
-(9, 'admin', 'admin@gmail.com', '6812f136d636e737248d365016f8cfd5139e387c', '1994-12-06', '1470002569');
+(14, 'admin', 'admin@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '2022-10-11', '0791668451');
 
 -- --------------------------------------------------------
 
@@ -107,9 +107,12 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`cid`, `name`, `email`, `phone`, `subject`, `message`) VALUES
-(8, 'Gideon Kiplangat', 'gideonkiplangat4@gmail.com', '0705647867', 'Homes', 'I need a house'),
 (9, 'Pauline Chelangat', 'pauline@gmail.com', '0734678908', 'Homes', 'I have a problem in finding a home of my desire.'),
-(10, 'james oyombe', 'james.oyombe@riarauniversity.ac.ke', '0705647867', 'Booking', 'I need to  book a room');
+(10, 'james oyombe', 'james.oyombe@riarauniversity.ac.ke', '0705647867', 'Booking', 'I need to  book a room'),
+(11, 'test', 'test@gmail.com', '0705647867', 'Booking', 'I want to book a room'),
+(12, 'Noel', 'noel@gmail.com', '0705123456', 'Homes', 'I need a home to buy'),
+(13, 'test', 'ahmed@gmail.com', '+254702499', 'Booking', 'dfk[;lludfszrdxfjlkljhgf'),
+(14, 'Evans', 'evans@gmail.com', '0734678908', 'Homes', 'I am looking for a mansion');
 
 -- --------------------------------------------------------
 
@@ -170,10 +173,10 @@ CREATE TABLE `property` (
 --
 
 INSERT INTO `property` (`pid`, `title`, `pcontent`, `type`, `bhk`, `stype`, `bedroom`, `bathroom`, `balcony`, `kitchen`, `hall`, `floor`, `size`, `price`, `location`, `city`, `state`, `feature`, `pimage`, `pimage1`, `pimage2`, `pimage3`, `pimage4`, `uid`, `status`, `mapimage`, `topmapimage`, `groundmapimage`, `totalfloor`, `date`, `isFeatured`) VALUES
-(25, 'Zills Home', '', 'house', '2 BHK', 'sale', 4, 2, 0, 1, 1, '5th Floor', 1869, 219690, '34-100 Zanzibar', 'Zanzibar', 'Tanzania', '<p>&nbsp;</p>\r\n<!---feature area start--->\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Property Age : </span>10 Years</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Swiming Pool : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Parking : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">GYM : </span>Yes</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Type : </span>Appartment</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Security : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Dining Capacity : </span>10 People</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Church/Temple : </span>Yes</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">3rd Party : </span>No</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Elevator : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">CCTV : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Water Supply : </span>Ground Water / Tank</li>\r\n</ul>\r\n</div>\r\n<!---feature area end---->\r\n<p>&nbsp;</p>', 'banner 2.jpg', 'pic4.jpg', 'banner 1.jpg', 'main.jpg', 'banner 1.jpg', 30, 'available', '', '', '', '11 Floor', '2022-07-22 22:29:20', 1),
 (30, 'Eastland Estate- Jogoo Road', '<p>Here We have 1,2 and 3Bedroom houses ranging from Kshs 25k-90k. Water and Security is well catered for, Our homes include small piece of land for growing short period crops.</p>', 'flat', '1 BHK', 'sale', 2, 2, 2, 1, 0, '3rd Floor', 30, 35, 'Jogoo Road', 'Nairobi', 'Kenya', '<p>&nbsp;</p>\r\n<!---feature area start--->\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Property Age : </span>10 Years</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Swiming Pool : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Parking : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">GYM : </span>Yes</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Type : </span>Apartment</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Security : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Dining Capacity : </span>10 People</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Church/Temple : </span>No</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">3rd Party : </span>No</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Elevator : No</span></li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">CCTV : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Water Supply : </span>Ground Water / Tank</li>\r\n</ul>\r\n</div>\r\n<!---feature area end---->\r\n<p>&nbsp;</p>', 'banner 3.jpg', 'banner 1.jpg', 'pic 2.jpg', 'pic 3.jpg', 'pic5.jpg', 36, 'available', '', '', '', '14 Floor', '2022-10-26 22:29:33', 1),
 (31, 'Canaan Estate-Mbagathi Way', '<p>It is a two-bedroom house</p>', 'apartment', '2 BHK', 'rent', 2, 2, 1, 1, 0, '4th Floor', 20, 25, 'Langata', 'Nairobi', 'Kenya', '<p>&nbsp;</p>\r\n<!---feature area start--->\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Property Age : </span>10 Years</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Swiming Pool : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Parking : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">GYM : </span>Yes</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Type : </span>Apartment</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Security : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Dining Capacity : </span>10 People</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Church/Temple : </span>No</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">3rd Party : </span>No</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Elevator : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">CCTV : No</span></li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Water Supply : </span>Ground Water / Tank</li>\r\n</ul>\r\n</div>\r\n<!---feature area end---->\r\n<p>&nbsp;</p>', 'banner 5.jpg', 'main.jpg', 'pic5.jpg', 'banner 1.jpg', 'pic 3.jpg', 38, 'available', 'pic 3.jpg', '', '', '5 Floor', '2022-10-27 12:13:07', 1),
-(32, 'J-Aprtments- Parklands', '<p>J- Apartments is a group of accommodation rooms available for customers either for leasing or buying</p>', 'apartment', '2 BHK', 'rent', 2, 1, 1, 1, 0, '3rd Floor', 20, 35000, 'Parklands', 'Nairobi', 'Kenya', '<p>&nbsp;</p>\r\n<!---feature area start--->\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Property Age : </span>10 Years</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Swiming Pool : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Parking : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">GYM : </span>Yes</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Type : </span>Apartment</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Security : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Dining Capacity : </span>10 People</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Church/Temple : </span>No</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">3rd Party : </span>No</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Elevator : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">CCTV : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Water Supply : </span>Ground Water / Tank</li>\r\n</ul>\r\n</div>\r\n<!---feature area end---->\r\n<p>&nbsp;</p>', 'banner 3.jpg', 'banner 5.jpg', 'pic 2.jpg', 'pic 2.jpg', 'pic4.jpg', 39, 'available', 'pic 3.jpg', '', 'house-floor-plan.png', '14 Floor', '2022-10-28 11:26:09', 1);
+(32, 'J-Aprtments- Parklands', '<p>J- Apartments is a group of accommodation rooms available for customers either for leasing or buying</p>', 'apartment', '2 BHK', 'rent', 2, 1, 1, 1, 0, '3rd Floor', 20, 35000, 'Parklands', 'Nairobi', 'Kenya', '<p>&nbsp;</p>\r\n<!---feature area start--->\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Property Age : </span>10 Years</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Swiming Pool : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Parking : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">GYM : </span>Yes</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Type : </span>Apartment</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Security : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Dining Capacity : </span>10 People</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Church/Temple : </span>No</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">3rd Party : </span>No</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Elevator : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">CCTV : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Water Supply : </span>Ground Water / Tank</li>\r\n</ul>\r\n</div>\r\n<!---feature area end---->\r\n<p>&nbsp;</p>', 'banner 3.jpg', 'banner 5.jpg', 'pic 2.jpg', 'pic 2.jpg', 'pic4.jpg', 39, 'available', 'pic 3.jpg', '', 'house-floor-plan.png', '14 Floor', '2022-10-28 11:26:09', 1),
+(33, 'Riara Apartments', '<p>The apartments has two bedroom</p>', 'apartment', '2 BHK', 'rent', 2, 2, 1, 1, 0, '2nd Floor', 20, 35000, 'Mbagathi way', 'Nairobi', 'Kenya', '<p>&nbsp;</p>\r\n<!---feature area start--->\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Property Age : </span>10 Years</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Swiming Pool : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Parking : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">GYM : </span>Yes</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Type : </span>Apartment</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Security : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Dining Capacity : </span>10 People</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Church/Temple : </span>No</li>\r\n</ul>\r\n</div>\r\n<div class=\"col-md-4\">\r\n<ul>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">3rd Party : </span>No</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Elevator : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">CCTV : </span>Yes</li>\r\n<li class=\"mb-3\"><span class=\"text-secondary font-weight-bold\">Water Supply : </span>Ground Water / Tank</li>\r\n</ul>\r\n</div>\r\n<!---feature area end---->\r\n<p>&nbsp;</p>', 'banner 2.jpg', 'pic4.jpg', 'banner 5.jpg', 'pic 2.jpg', 'pic5.jpg', 40, 'available', '', '', '', '10 Floor', '2022-10-31 11:56:15', 1);
 
 -- --------------------------------------------------------
 
@@ -216,14 +219,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`uid`, `uname`, `uemail`, `uphone`, `upass`, `utype`, `uimage`) VALUES
-(28, 'Christine', 'christine@mail.com', '7777444455', '6812f136d636e737248d365016f8cfd5139e387c', 'user', 'gr7.png'),
-(30, 'Thomas Olson', 'thomas@mail.com', '7896665555', '6812f136d636e737248d365016f8cfd5139e387c', 'user', 'avatarm7-min.jpg'),
-(33, 'Noah Stones', 'noah@mail.com', '7965555544', '6812f136d636e737248d365016f8cfd5139e387c', 'user', 'usersys-min.png'),
-(34, 'Fred Godines', 'fred@mail.com', '7850002587', '6812f136d636e737248d365016f8cfd5139e387c', 'builder', 'user-a-min.png'),
 (35, 'Michael', 'michael@mail.com', '8542221140', '6812f136d636e737248d365016f8cfd5139e387c', 'user', 'usric.png'),
 (36, 'Ikran', 'ikran@gmail.com', '0791668451', 'f30cf15abf57e04c1a9d8ee9c55b714a7597b249', 'agent', 'avatar 1.jpg'),
 (37, 'test', 'test@gmail.com', '0705647867', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'user', 'avatar 1.jpg'),
-(39, 'james oyombe', 'james.oyombe@riarauniversity.ac.ke', '0705647867', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'agent', 'avatar 1.jpg');
+(40, 'Mimi', 'mimi@gmail.com', '0705647867', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'agent', 'avatar 1.jpg');
 
 --
 -- Indexes for dumped tables
@@ -291,7 +290,7 @@ ALTER TABLE `about`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `city`
@@ -303,7 +302,7 @@ ALTER TABLE `city`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `feedback`
@@ -315,7 +314,7 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT for table `property`
 --
 ALTER TABLE `property`
-  MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `state`
@@ -327,7 +326,7 @@ ALTER TABLE `state`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
